f1091f64d7bb35266be005c6608beee4580eb039
K1ngHandy <bolds-slicks0g@icloud.com>
2025-05-28 21:49:38 -0400

    Session 1

---
+++ test1.py
@@ -0,0 +1,19 @@
+print("West Chester")
+print("CSC141")
+print(' WCUPA ')
+
+#Print quote with \ or '''
+print(' I\'m here ')
+print(''' I'm here ''')
+print(" \" \' ")
+
+##Variables
+a=10
+#print(a)
+a=-1
+#print(a)
+b=2
+print(a+b) # -1 + 2
+print(b+b)
+c=100
+print(c+b)
