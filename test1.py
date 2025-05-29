60bd05f361261199b885038ec8671c8927517005
K1ngHandy <bolds-slicks0g@icloud.com>
2025-05-28 22:14:26 -0400

    add comments

--- test1.py
+++ test1.py
@@ -2,12 +2,12 @@ print("West Chester")
 print("CSC141")
 print(' WCUPA ')
 
-#Print quote with \ or '''
+# Print quote with \ or '''
 print(' I\'m here ')
 print(''' I'm here ''')
 print(" \" \' ")
 
-##Variables
+## Variables
 a=10
 #print(a)
 a=-1
@@ -17,3 +17,16 @@ print(a+b) # -1 + 2
 print(b+b)
 c=100
 print(c+b)
+# Cannot start var name with num
+a2=100
+print(a2)
+int=1.234 # float double
+print(int)
+
+# Add 1 to A
+A=10
+print(A+1)
+
+print(a,b,A) # print multiple on line
+print(a,"\n",b) # new line
+print(a,"\t",b) # insert tab
