29f2a6e4666cf1b667fdae1c0c00637dc364106e
K1ngHandy <bolds-slicks0g@icloud.com>
2025-05-30 20:47:50 -0400

    update test4

--- test4.py
+++ test4.py
@@ -3,8 +3,17 @@ print(number**2, number**3)
 print(number*number, "\t", number**2) # tab inserted
 
 ## Area of a triangle A=(side*h/2)
-Side=float(input("What is length of a side?"))
-HG=float(input("What is the height?"))
-Area=Side*HG/2
-print("Area is", Area)
-print("The area is", Side*HG/2)
+Side=float(input("What is length of a side? "))
+HG=float(input("What is the height? "))
+AreaTriangle=Side*HG/2
+print("Triangle area is", AreaTriangle)
+print("Triangle area is", Side*HG/2)
+
+## Area of a circle A=Pi*R*R
+R=float(input("Enter radius: "))
+AreaCircle=3.14*R*R
+print("Circle Area is:", AreaCircle)
+
+## Get name and display 4 times
+name=input("What is your name? ")
+print(name*3)
