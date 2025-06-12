2ffefc42b3ac50327587ca0ebd6aace712922d3a
K1ngHandy <bolds-slicks0g@icloud.com>
2025-06-11 23:26:26 -0400

    move large files to computer

---
+++ assignments/week2/Test1-CSC141.py
@@ -0,0 +1,23 @@
+## 1
+##x = 20
+##print("There are " ,x*5 , "\n" +"hens in the hen house.")
+
+## 2
+##x = 22
+##y = 4
+##y = y + x
+##print("x = " , x , ", y = " , y )
+
+## 3
+##print(1+2*3%3-6)
+
+## 4
+##x = 12345.45
+##print(f'{x:7,.1f}')
+
+## 5
+import random
+num = random.randint(1,10)
+a = 5
+num = 11
+print(num / a)
---
+++ assignments/week3/hw2.py
@@ -0,0 +1,11 @@
+## Stephen Handy
+## CSC141
+# Homework #2
+
+## 1 - Write a program to get a number between 1-12 and determine the corresponding month
+
+## 2 - Write a program to get a person’s age and determine the ticket price. 
+    # $2 for a child (no more than 10 years), $3 for a senior (at least 65 years), or $5 for an adult.
+
+## 3 - Write a program to get a date and determine the day of the week.
+    # To determine the day of the week, given the month, day, and year, you can use Zeller’s formula.
