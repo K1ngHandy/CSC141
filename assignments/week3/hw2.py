61f22793230667cb71e346379925aa0eefdc7ca9
K1ngHandy <bolds-slicks0g@icloud.com>
2025-06-12 03:08:57 -0400

    add day of week using Zeller's congruence

--- assignments/week3/hw2.py
+++ assignments/week3/hw2.py
@@ -32,9 +32,39 @@ else:
     else:
         print("December")
 
-
 ## 2 - Write a program to get a person’s age and determine the ticket price. 
     # $2 for a child (no more than 10 years), $3 for a senior (at least 65 years), or $5 for an adult.
+age = int(input("Enter your age: "))
+if (age < 1):
+    print("Invalid age")
+elif(age <= 10):
+    print("Child ticket price: $2")
+elif(age >= 65):
+    print("Senior ticket price: $3")
+else:
+    print("Adult ticket price: $5")
 
 ## 3 - Write a program to get a date and determine the day of the week.
     # To determine the day of the week, given the month, day, and year, you can use Zeller’s formula.
+    # want MM/DD/YYYY as input reques and output day of the week using Zeller's congruence
+date = input ("Enter a date (MM/DD/YYYY): ")
+m, q, y = map(int, date.split('/'))
+# Zeller's Congruence, where m is month, q is day of the month, and y is the year.
+h = (q + ((13 * (m + 1)) // 5) + y % 100 + (y % 100) // 4 + (y // 100) // 4 - 2 * (y // 100)) % 7
+
+if (h == 0):
+    h = "Saturday"
+elif(h == 1):
+    h = "Sunday"
+elif(h == 2):
+    h = "Monday"
+elif(h == 3):
+    h = "Tuesday"
+elif(h == 4):
+    h = "Wednesday"
+elif(h == 5):
+    h = "Thursday"
+elif(h == 6):
+    h = "Friday"
+
+print("Day of the week is:", h)
