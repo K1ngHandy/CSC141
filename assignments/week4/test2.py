8ca73f8019201034348adaebe7bcb9b992386c64
K1ngHandy <bolds-slicks0g@icloud.com>
2025-06-16 13:34:56 -0400

    test2 1-3

---
+++ assignments/week4/test2.py
@@ -0,0 +1,36 @@
+## 1 - What would be displayed as the result of executing the following code?
+x = -1
+y = 0
+
+x = x + 1
+y = x + y
+print("1y", y)
+# Answer: 0
+
+# 2 - What would be displayed as the result of executing the following code?
+x = 10
+y = 12
+
+if (x % 2 == 0) and (y % 5 == 0):
+    print("2y:", y)
+else:
+    print("2x", x)
+
+# 3 - What would be displayed as the result of executing the following code?
+x = 10
+y = 12
+
+if (x % 2 != 0):
+    print("3y", y)
+elif (x > 15):
+    print("3x", x)
+else:
+    print("****")
+# Answer: ****
+
+# 4 - What would be displayed as the result of executing the following code?
+x = 1234.55
+print(f'{x:^10.1f}', 5)
+# Answer: '  1234.6 5'
+
+# 5 - What would be displayed as the result of executing the following code?
