656e2ac6d25dd0c85c208a23196f57c8af8f50be
K1ngHandy <bolds-slicks0g@icloud.com>
2025-06-16 00:50:06 -0400

    looping

---
+++ notes/week4/notes4.py
@@ -0,0 +1,16 @@
+# week4 notes
+
+## while loops
+m = 1 # initial condition
+while(m < 5):
+    print(m) # body of loop
+    m = m + 1 # update condition
+
+## total of 1, 2, 3, 4, 5
+n = 1
+total = 0
+while (n < 6):
+    total = total + n
+    n = n + 1
+    print(total)
+# print(total) # final total instead of looping total when at start of line
