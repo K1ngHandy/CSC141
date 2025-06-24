2220684be1ac6803770e846c5be4871584fc2f06
Stephen Handy <bolds-slicks0g@icloud.com>
2025-06-24 12:19:54 -0400

    add month converter

---
+++ notes/week5/notes5.py
@@ -0,0 +1,29 @@
+# Number in bounds
+num = int(input("Enter a number between 1-12: "))
+if (num == 1):
+    print("January")
+elif (num == 2):
+    print("February")
+elif (num == 3):
+    print("March")
+elif (num == 4):
+    print("April")
+elif (num == 5):
+    print("May")
+elif (num == 6):
+    print("June")
+elif (num == 7):
+    print("July")
+elif (num == 8):
+    print("August")
+elif (num == 9):
+    print("September")
+elif (num == 10):
+    print("October")
+elif (num == 11):
+    print("November")
+elif (num == 12):
+    print("December")
+else:
+    print("Number not in bounds")
+
