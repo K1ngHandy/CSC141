44091d2a4a1de18ef7aef62ab7c50904bc38f490
Stephen Handy <bolds-slicks0g@icloud.com>
2025-06-24 16:23:03 -0400

    nested loop

--- assignments/week5/hw3-2.py
+++ assignments/week5/hw3-2.py
@@ -1,1 +1,15 @@
-# 2 - Generate pattern with nested loops
+# 2 - Generate pattern with nested loops:
+# 1 1 1
+#  2 2
+# 3 3 3
+#  4 4
+# 5 5 5
+
+n = 1
+while(n <= 5):
+    if (n % 2 == 0):
+        print('', n, n, '')
+        n = n + 1
+    else:
+        print(n, n, n)
+        n = n + 1
